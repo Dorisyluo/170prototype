14ad74c6a31f882538baa94a7dd47ac6e8ff50fe
Giancarlo Tomasello <gtomasel@ucsc.edu>
2019-10-27 11:48:32 -0700

    Updated Main
    Added game over state to state machine

--- bartender/js/main.js
+++ bartender/js/main.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Game');
 
 game.state.add('Play', Play);
+game.state.add('GameOver', GameOver);
 game.state.start('Play');
 
 // function preload() {
